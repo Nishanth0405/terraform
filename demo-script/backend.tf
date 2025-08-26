22659b36cba910fb172ae9518a6b7b4d3a7d599d
Nishanth0405 <rn05062001@gmail.com>
2025-08-26 10:29:32 +0530

    added S3 store tf file

--- demo-script/backend.tf
+++ demo-script/backend.tf
@@ -1,6 +1,8 @@
-# terraform {
-#   backend "s3" {
-
-#     prefix = "crato"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "terraform-nish-demo" 
+    key            = "demo/terraform.tfstate"        
+    region         = "ap-south-1"                              
+    encrypt        = true                           
+  }
+}
